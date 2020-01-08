367ee36f37a07e8ef20d10a2a33c189b29d3867d
Alejandro Perez <alex2721ap@gmail.com>
2020-01-08 15:06:59 -0800

    Small Changes to bot

--- redditBot.py
+++ redditBot.py
@@ -2,10 +2,12 @@ import praw
 
 reddit = praw.Reddit('bot1')
 
-subreddit = reddit.subreddit("learningpython")
+subreddit = reddit.subreddit("monsterhunter")
 
 for submission in subreddit.hot(limit=5):
     print('Title: ', submission.title)
     print('Text: ', submission.selftext)
     print("Score: ", submission.score)
-    print("-------------------------\n")
+    print("URL: ", submission.url)
+    print("Visited: ", submission.visited)
+    print("--------------------------------------\n")
